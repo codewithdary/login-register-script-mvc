81e160b0c97963f7903ab4956063f0b63fa526e5
Darynazar <63154066+Darynazar@users.noreply.github.com>
2020-09-21 12:20:17 +0200

    Update navigation.php

--- mvcloginregister/app/views/includes/navigation.php
+++ mvcloginregister/app/views/includes/navigation.php
@@ -1,19 +1,19 @@
 <nav class="top-nav">
     <ul>
         <li>
-            <a href="<?php echo URLROOT; ?>/pages/index">Home</a>
+            <a href="<?php echo URLROOT; ?>/index">Home</a>
         </li>
         <li>
-            <a href="<?php echo URLROOT; ?>/pages/about">About</a>
+            <a href="<?php echo URLROOT; ?>/about">About</a>
         </li>
         <li>
-            <a href="<?php echo URLROOT; ?>/pages/projects">Projects</a>
+            <a href="<?php echo URLROOT; ?>/projects">Projects</a>
         </li>
         <li>
             <a href="<?php echo URLROOT; ?>/posts">Blog</a>
         </li>
         <li>
-            <a href="<?php echo URLROOT; ?>/pages/contact">Contact</a>
+            <a href="<?php echo URLROOT; ?>/contact">Contact</a>
         </li>
         <li class="btn-login">
             <?php if(isset($_SESSION['user_id'])) : ?>

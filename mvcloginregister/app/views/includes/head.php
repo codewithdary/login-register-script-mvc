4dc4afb8ced12bdedc1dcaab59b8116545870a6c
znix <64723880+znixhook@users.noreply.github.com>
2020-10-03 10:22:11 +0530

    Fixed typo

--- mvcloginregister/app/views/includes/head.php
+++ mvcloginregister/app/views/includes/head.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <meta htttp-equiv="Cache-control" content="no-cache">
+    <meta http-equiv="Cache-control" content="no-cache">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo SITENAME; ?></title>
     <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/style.css">
